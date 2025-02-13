4ea66f4d2a81b727b803c4d432cc60b57eef43d2
xiaopengli <82141822+lxpqxl@users.noreply.github.com>
2025-02-13 20:18:11 +0800

    Add files via upload

---
+++ whitenlearn.m
@@ -0,0 +1,46 @@
+function Lw = whitenlearn (x, qidxs, pidxs)
+% WHITENLEARN learns discriminative whitening on given data.
+%  Lw = whitenlearn (X, QIDXS, PIDXS)
+%
+%   Learn discriminative whitening Lw using annotations
+%   
+%   Input: 
+%     X     : descriptors 
+%     QIDXS : query idxs
+%     PIDXS : positive idxs
+%   
+%   Output:
+%     Lw.P   : Whitening projection, learned on query-pos pairs
+%     Lw.m   : Mean of query descriptors, for centering
+%
+% Authors: F. Radenovic, G. Tolias, O. Chum. 2017. 
+
+tic;
+D = size(x,1);
+N = size(x,2);
+
+%% Learning Lw w annotations
+
+m = mean(x(:,qidxs),2);
+
+df = x(:,qidxs) - x(:,pidxs);
+
+S = df * df' ./ size(df, 2);
+
+P = inv(chol(S))';
+
+df = P*(x-m);
+
+D = df * df'./N;
+
+[V, eigval] = eig(D);
+
+[~, ord] = sort(diag(eigval),'descend');
+
+V = V(:,ord);
+
+Lw.P = V' *P;
+
+Lw.m = m;
+
+toc;
